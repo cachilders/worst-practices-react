b1c51400b6f038fda8059fc21c2549bdeed76a2b
Casey Childers <cachilders@gmail.com>
2017-01-12 12:22:52 -0800

    chore(webpack): Configure basic dev build variation

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -5,91 +5,101 @@ const { resolve } = require('path');
 
 const home = resolve(__dirname, '../');
 
-module.exports = {
-  entry: {
-    app: `${home}/src/app/index.jsx`,
-    vendor: [
-      'axios',
-      'immutable',
-      'react',
-      'react-dom',
-      'redux',
-      'redux-thunk',
-      'semantic-ui-react',
-    ],
-  },
-  output: {
-    filename: '[hash].[name].js',
-    path: `${home}/dist`,
-    publicPath: '/',
-  },
-  context: `${home}/src`,
-  devtool: 'inline-source-map',
-  devServer: {
-    hot: true,
-    contentBase: `${home}/dist`,
-    publicPath: '/',
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.es6'],
-  },
-  module: {
-    rules: [
-      {
-        exclude: /node_modules/,
-        test: /\.js[x]?|\.es6$/,
-        use: 'babel-loader',
-        query: { compact: false },
-      },
-      {
-        test: /\.css$/,
-        use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader' },
-        ],
-      },
-      {
-        test: /\.scss$/,
-        loader: ExtractTextPlugin.extract({
-          fallbackLoader: 'style-loader',
-          loader: 'css-loader!sass-loader',
-        }),
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        loader: 'url-loader',
-        options: {
-          limit: 1000, // 1KO
-          name: 'img/[name].[ext]?[hash]',
+module.exports = (options) => {
+  const webpackConfig = {
+    entry: {
+      app: `${home}/src/app/index.jsx`,
+      vendor: [
+        'axios',
+        'immutable',
+        'react',
+        'react-dom',
+        'redux',
+        'redux-thunk',
+        'semantic-ui-react',
+      ],
+    },
+    output: {
+      filename: '[hash].[name].js',
+      path: `${home}/dist`,
+      publicPath: '/',
+    },
+    context: `${home}/src`,
+    devtool: 'inline-source-map',
+    resolve: {
+      extensions: ['.js', '.jsx', '.es6'],
+    },
+    module: {
+      rules: [
+        {
+          exclude: /node_modules/,
+          test: /\.js[x]?|\.es6$/,
+          use: 'babel-loader',
+          query: { compact: false },
         },
-      },
-      {
-        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url-loader',
-        query: {
-          limit: 1000, // 1 KO
-          name: 'fonts/[name].[hash:7].[ext]',
+        {
+          test: /\.css$/,
+          use: [
+            { loader: 'style-loader' },
+            { loader: 'css-loader' },
+          ],
         },
-      },
+        {
+          test: /\.scss$/,
+          loader: ExtractTextPlugin.extract({
+            fallbackLoader: 'style-loader',
+            loader: 'css-loader!sass-loader',
+          }),
+        },
+        {
+          test: /\.(png|jpg|gif|svg)$/,
+          loader: 'url-loader',
+          options: {
+            limit: 1000, // 1KO
+            name: 'img/[name].[ext]?[hash]',
+          },
+        },
+        {
+          test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+          loader: 'url-loader',
+          query: {
+            limit: 1000, // 1 KO
+            name: 'fonts/[name].[hash:7].[ext]',
+          },
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({ template: `${home}/src/index.html` }),
+      new Webpack.optimize.CommonsChunkPlugin({
+        async: true,
+        children: true,
+        names: ['vendor', 'manifest'],
+      }),
+      new ExtractTextPlugin({
+        filename: 'styles.css',
+        allChunks: true,
+      }),
+      new Webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(options.NODE_ENV),
+      }),
+      new Webpack.ProvidePlugin({
+        // Make stuff available here
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({ template: `${home}/src/index.html` }),
-    new Webpack.optimize.CommonsChunkPlugin({
-      async: true,
-      children: true,
-      names: ['vendor', 'manifest'],
-    }),
-    new ExtractTextPlugin({
-      filename: 'styles.css',
-      allChunks: true,
-    }),
-    new Webpack.ProvidePlugin({
-    }),
-    new Webpack.HotModuleReplacementPlugin(),
-    new Webpack.NamedModulesPlugin(),
-    new Webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
-    }),
-  ],
+  };
+
+  if (options.NODE_ENV === 'development') {
+    webpackConfig.plugins.push(
+      new Webpack.HotModuleReplacementPlugin()
+    );
+
+    webpackConfig.devServer = {
+      hot: true,
+      contentBase: `${home}/dist`,
+      publicPath: '/',
+    };
+  }
+
+  return webpackConfig;
 };
---
+++ webpack/webpack.dev.js
@@ -0,0 +1,3 @@
+module.exports = require('./webpack.config')({
+  NODE_ENV: 'development',
+});
